68e4e6f8cd3d72a21f986d9291764ffa99f9e5d3
MuriloBertelli <mrlbertelli@gmail.com>
2026-08-14 19:55:19 -0300

    utilitarios

--- 00-c-refresher/relembrando_c.c
+++ 00-c-refresher/relembrando_c.c
@@ -13,6 +13,17 @@ static registro *historico = NULL;
 static size_t qtd_regs = 0;
 static size_t capacidade = 0;
 
+void limpar_buffer(void);
+double *alocar_memoria(size_t n);
+int ler_double(double *msg, double *destino);
+int ler_int(int *valor);
+void registrar(const char *msg, double resultado);
+void mostrar_historico(void);
+void liberar_historico(void);
+void imprimir_polinomio(const double *coef, int grau, const char *rotulo);
+
+
+
 int main ()
 {
     return 0;
